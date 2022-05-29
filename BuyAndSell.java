6c79269188d8f9455fe973753f5d0eaab26daf18
PRABHAT747 <71284979+PRABHAT747@users.noreply.github.com>
2022-05-29 17:07:58 +0530

    BuyAndSell.java

---
+++ BuyAndSell.java
@@ -0,0 +1,26 @@
+public class BuyAndSell {
+    public static void main(String[] args) {
+
+
+ int arr[]={7,1,5,3,6,4};
+ int N=arr.length;
+ int maxtillnow[]=new int[N];
+ int tempmax=arr[N-1];
+      //using a prefix arr
+ for(int i=N-1;i>=0;i--){
+     if(arr[i]>tempmax)
+         tempmax=arr[i];
+     maxtillnow[i]=tempmax;
+ }
+        System.out.println(Arrays.toString(maxtillnow));
+ int maxprofit=0;
+ for(int i=0;i<arr.length;i++){
+     maxprofit=Math.max(maxprofit,maxtillnow[i]-arr[i]);
+    }
+        System.out.println(maxprofit);
+ }
+
+    }
+//time complexity O(N)
+//space used O(N)
+
